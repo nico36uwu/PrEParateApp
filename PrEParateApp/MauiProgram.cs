ab975e861f9e7781dff19473f57720d400d9d8e9
nfispas <nf.ispas@soprasteria.com>
2024-04-17 10:57:35 +0200

    Configuracion paquetes nuget net maui

--- PrEParateApp/MauiProgram.cs
+++ PrEParateApp/MauiProgram.cs
@@ -1,26 +1,28 @@
 ﻿using Microsoft.Extensions.Logging;
 using Supabase;
+using CommunityToolkit.Maui;
 
-namespace PrEParateApp
+namespace PrEParateApp;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-            //Client.Initialize(null,null);
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+            .UseMauiCommunityToolkit()
+            // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-            return builder.Build();
-        }
+        // Continue initializing your .NET MAUI App here
+
+
+        return builder.Build();
     }
 }
---
+++ PrEParateApp/Model/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrEParateApp.Model
+{
+    public static class AppConfig
+    {
+        public const string SUPABASE_URL = "";
+        public const string SUPABASE_KEY = "";
+    }
+}
